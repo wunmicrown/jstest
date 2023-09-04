72e5045336200c714f02ab030d3aba901eaf6011
wunmicrown <olayiwolapamilerin34@gmail.com>
2023-09-04 18:35:55 +0100

    added

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ console.log(false);
 console.log(false);
 console.log(false);
 console.log(false);
+console.log(true);
+console.log(false);
+console.log(true);
 function use(name) {
     alert('This is ' + name)
   }
@@ -20,6 +23,15 @@ function cards(name) {
         alert('Incorrect details')
       }
   }
+let computer= 'software engineering'
+let adminn= 'pamilerin'
+function privilege() {
+  if (program.value ==computer && admin.value==adminn) { 
+      alert('Logged in')
+    }else {
+      alert('Incorrect details ')
+    }
+}
   let age=18
   let country="Nigeria"
  function reg() {
