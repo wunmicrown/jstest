03acf26ca4900e9c2eb9f346f422db78f13b344d
wunmicrown <olayiwolapamilerin34@gmail.com>
2023-09-04 17:50:02 +0100

    added

---
+++ index.js
@@ -0,0 +1,31 @@
+console.log(true);
+console.log(false);
+console.log(true);
+console.log(false);
+console.log(false);
+console.log(false);
+console.log(false);
+function use(name) {
+    alert('This is ' + name)
+  }
+function cards(name) {
+    alert('This is ' + name)
+  }
+  let recovery= 'white'
+  let favority= 'sky'
+  function security() {
+    if (system.value ==recovery && petname.value== favority) { 
+        alert('password has changed')
+      }else {
+        alert('Incorrect details')
+      }
+  }
+  let age=18
+  let country="Nigeria"
+ function reg() {
+    if(age.value >=18  && country.value == "Nigeria") {
+        alert("your vote is valid")
+    }else(
+        alert("your vote is not valid")
+    )
+ }
